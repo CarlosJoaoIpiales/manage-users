44fd377f4bf8bbf4d1c8f0f50c3a93b04d89d63a
Shadow_Moon <cjipiales1@espe.edu.ec>
2023-11-26 12:26:08 -0500

    Program web for managed users with model mvc with asp.net and c# for styles with use bootstrap and the connection to db we realize with smss

---
+++ MVCCRUD/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCCRUD.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    // Controller for what user not enter numbers in the textarea for just names
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo debe contener letras.")]
+    public string? Nombre { get; set; }
+
+    public DateOnly? Fecha { get; set; }
+
+    public string? Clave { get; set; }
+
+
+    
+}
